ab31288dd00a751d27fd2777bf60f53d97875be9
smoh <seungmuk92@gmail.com>
2023-08-04 13:04:43 +0900

    InitialCommit

---
+++ boDicom.WPF/DicomFrameInfo.cs
@@ -0,0 +1,21 @@
+﻿using FellowOakDicom.Imaging;
+using FellowOakDicom.IO.Buffer;
+using Microsoft.Toolkit.HighPerformance.Buffers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace boDicom.WPF
+{
+    public class DicomFrameInfo
+    {
+        public int FrameNumber { get; set; }
+        public long FrameOffset { get; set; }
+        public long FrameSize { get; set; }
+    }
+}
