9c970a6444ce545dd9c176816334cefbe777944b
smoh <seungmuk92@gmail.com>
2025-12-02 22:21:49 +0900

    MAUI 프로젝트 제거.

--- boDicom.MAUI/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using boDicom.MAUI.Data;
-global using boDicom.MAUI.PageModels;
-global using boDicom.MAUI.Pages;
-global using boDicom.MAUI.Services;
-global using boDicom.MAUI.Utilities;
-global using Fonts;
